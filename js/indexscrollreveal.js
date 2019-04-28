48415c1e228ad905aceaa4c3d4a659c76bd331f7
Julien <jl.julien.leonard@gmail.com>
2019-04-28 12:12:33 +0800

    add scrollreveal

---
+++ js/indexscrollreveal.js
@@ -0,0 +1,49 @@
+ScrollReveal().reveal('.intro',{
+    delay: 500,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.image_full',{
+    delay: 1500,
+    duration: 5000,
+    scale: 0.99,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.quotation',{
+    delay: 500,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.quoteauthor',{
+    delay: 500,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.paragraph',{
+    delay: 1500,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.workdescriptionright',{
+    delay: 1500,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.work-left-frame',{
+    delay: 1500,
+    useDelay: 'onload',
+    reset: true,
+});
+
+ScrollReveal().reveal('.work-left-frame',{
+    delay: 1500,
+    useDelay: 'onload',
+    reset: true,
+});
+ScrollReveal().reveal('.paratitle',{
+    delay: 1500,
+    useDelay: 'onload',
+    reset: true,
+});
+
